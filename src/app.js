fad3e825dbeb2169bff980daf71ef428f9434250
DavidCampsHz <davidcamposhz@gmail.com>
2022-01-11 11:38:26 +0000

    Adding code in app.js and adding the excuse tag in index.html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,56 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let article = "The ";
+  let adjective = [
+    "green ",
+    "big ",
+    "small ",
+    "mad ",
+    "dummy ",
+    "red ",
+    "hungry "
+  ];
+  let noun = ["dog ", "bird ", "car ", "cat ", "cloud ", "runner ", "woman "];
+  let verb = [
+    "stole ",
+    "ate ",
+    "punched ",
+    "broke ",
+    "wrote ",
+    "sent ",
+    "clicked "
+  ];
+  let thing = [
+    "my mobile ",
+    "your mice ",
+    "his socks ",
+    "her mask ",
+    "our glass ",
+    "my homework "
+  ];
+  let place = [
+    "at home",
+    "in the cinema",
+    "at the office",
+    "from my jacket",
+    "on the table",
+    "at the beach"
+  ];
+
+  // random variables
+  let randomAdj = Math.floor(Math.random() * adjective.length);
+  let randomNoun = Math.floor(Math.random() * noun.length);
+  let randomVerb = Math.floor(Math.random() * verb.length);
+  let randomThing = Math.floor(Math.random() * thing.length);
+  let randomPlace = Math.floor(Math.random() * place.length);
+
+  // concatenate the excuse
+  document.querySelector("#randomExcuses").innerHTML =
+    article +
+    adjective[randomAdj] +
+    noun[randomNoun] +
+    verb[randomVerb] +
+    thing[randomThing] +
+    place[randomPlace];
 };
