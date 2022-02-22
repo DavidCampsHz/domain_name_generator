f97dee77b7d1d0286bb113ca857e10c688b3929b
DavidCampsHz <davidcamposhz@gmail.com>
2022-02-22 12:20:34 +0000

    Fixing layout for suits

--- src/app.js
+++ src/app.js
@@ -1,62 +1,19 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+};
 
-window.onload = function() {
-  //write your code here
-  let article = "The ";
-  let adjective = [
-    "green ",
-    "big ",
-    "small ",
-    "mad ",
-    "dummy ",
-    "red ",
-    "hungry "
-  ];
-  let noun = ["dog ", "bird ", "car ", "cat ", "cloud ", "runner ", "woman "];
-  let verb = [
-    "stole ",
-    "ate ",
-    "punched ",
-    "broke ",
-    "wrote ",
-    "sent ",
-    "clicked "
-  ];
-  let thing = [
-    "my mobile ",
-    "your mice ",
-    "his socks ",
-    "her mask ",
-    "our glass ",
-    "my homework "
-  ];
-  let place = [
-    "at home",
-    "in the cinema",
-    "at the office",
-    "from my jacket",
-    "on the table",
-    "at the beach"
-  ];
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
 
-  // random variables
-  let randomAdj = Math.floor(Math.random() * adjective.length);
-  let randomNoun = Math.floor(Math.random() * noun.length);
-  let randomVerb = Math.floor(Math.random() * verb.length);
-  let randomThing = Math.floor(Math.random() * thing.length);
-  let randomPlace = Math.floor(Math.random() * place.length);
-
-  // concatenate the excuse
-  document.querySelector("#randomExcuses").innerHTML =
-    article +
-    adjective[randomAdj] +
-    noun[randomNoun] +
-    verb[randomVerb] +
-    thing[randomThing] +
-    place[randomPlace];
+let generateRandomSuit = () => {
+  let suits = ["diamond", "spade", "heart", "club"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
